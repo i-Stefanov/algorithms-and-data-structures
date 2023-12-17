c432408bfcea65dab204c28aa05fe0cdd352e704
i-Stefanov <122023382+i-Stefanov@users.noreply.github.com>
2023-12-17 18:53:20 +0200

    linkedList using classes

--- Linked lists/linkedList.js
+++ Linked lists/linkedList.js
@@ -1,3 +1,10 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class LinkedList {
   constructor(value) {
     this.head = {
@@ -8,10 +15,7 @@ class LinkedList {
     this.length = 1;
   }
   append(value) {
-    const newNode = {
-      value: value,
-      next: null,
-    };
+    const newNode = new Node(value);
     this.tail.next = newNode;
     this.tail = newNode;
 
@@ -19,20 +23,52 @@ class LinkedList {
     return this;
   }
   prepend(value) {
-    const newNode = {
-      value: value,
-      next: null,
-    };
+    const newNode = new Node(value);
+
     newNode.next = this.head;
     this.head = newNode;
 
     this.length++;
+  }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    console.log(array);
+    return array;
+  }
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  insert(value, index) {
+    if (index === 0) {
+      return this.prepend(value);
+    }
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    let newNode = new Node(value);
+    const previousNode = this.traverseToIndex(index - 1);
+    const nextNode = previousNode.next;
+    previousNode.next = newNode;
+    newNode.next = nextNode;
+    this.length++;
+    return this.printList();
   }
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
-myLinkedList.prepend(1);
-
-console.log(myLinkedList);
+myLinkedList.insert(22, 2);
+myLinkedList.insert(99, 99);
+myLinkedList.printList();
